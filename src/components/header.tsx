515a8925a7572fbd018b8e111ed1075be27a8458
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-12-25 23:13:06 -0800

    add face

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,13 @@
 import { Link } from "waku";
+import { Face } from "./face";
 
 export const Header = () => {
   return (
-    <header className="flex items-center gap-4 p-6 lg:fixed lg:left-0 lg:top-0">
+    <header className="flex items-center gap-4 p-6 lg:fixed lg:left-0 lg:top-0 lg:right-0 justify-between w-full">
       <h2 className="text-lg font-bold tracking-tight">
         <Link to="/">༼ ╹‿╹ ༽</Link>
       </h2>
+      <Face height={50} />
     </header>
   );
 };
