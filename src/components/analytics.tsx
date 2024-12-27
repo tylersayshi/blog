37edd6ac710606537a40b642df4a97febba02100
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-12-26 18:38:48 -0800

    chore: remove log

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -3,7 +3,6 @@ import { useRouter_UNSTABLE } from "waku";
 
 export const Analytics = () => {
   const router = useRouter_UNSTABLE();
-  console.log(router.path);
   return (
     <img
       src={`https://app.piratepx.com/ship?p=67d07cf8-d340-46cf-8560-6e9b462d1d7e&i=tylur.blog${router.path}`}
