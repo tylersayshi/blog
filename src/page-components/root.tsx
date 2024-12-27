654b71dc8460d23219a6e3765834191d9322fa61
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-12-26 18:35:29 -0800

    path based analytics

--- src/page-components/root.tsx
+++ src/page-components/root.tsx
@@ -6,15 +6,7 @@ export function Root({ children }: { children: ReactNode }) {
       <head>
         <title>tyler&apos;s blog</title>
       </head>
-      <body>
-        {children}
-        <img
-          src="https://app.piratepx.com/ship?p=67d07cf8-d340-46cf-8560-6e9b462d1d7e&i=tylur.blog"
-          aria-hidden="true"
-          style={{ display: "none" }}
-          suppressHydrationWarning
-        />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
