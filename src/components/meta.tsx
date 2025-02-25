40dfd378da3ca37c25831ad04a1e593ba5b2bd38
Tyler <26290074+thegitduck@users.noreply.github.com>
2025-02-24 20:51:29 -0800

    I think it should work now

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -8,6 +8,12 @@ export const Meta = ({ title, description, path }: MetaProps) => {
   return (
     <>
       <title>{title}</title>
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="https://tylur.blog/tylur.svg" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="tylur.blog" />
+      <meta name="twitter:image" content="https://tylur.blog/tylur.svg" />
       <meta
         property="og:url"
         content={"https://tylur.blog" + (path === "/" ? "" : path)}
--- src/page-components/layout.tsx
+++ src/page-components/layout.tsx
@@ -12,24 +12,15 @@ export async function RootLayout({ children }: RootLayoutProps) {
   const data = await getData();
 
   return (
-    <>
-      <meta property="og:type" content="website" />
-      <meta property="og:image" content="/tylur.svg" />
-
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="tylur.blog" />
-      <meta name="twitter:image" content="/tylur.svg" />
-
-      <div className="font-sans">
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content={data.description} />
-        <Header />
-        <main className="m-6 flex lg:py-12 *:min-h-64 *:min-w-64 lg:m-0 min-h-[calc(100vh-214px)] lg:min-h-svh justify-center">
-          <PostHogProvider>{children}</PostHogProvider>
-        </main>
-        <Footer />
-      </div>
-    </>
+    <div className="font-sans">
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content={data.description} />
+      <Header />
+      <main className="m-6 flex lg:py-12 *:min-h-64 *:min-w-64 lg:m-0 min-h-[calc(100vh-214px)] lg:min-h-svh justify-center">
+        <PostHogProvider>{children}</PostHogProvider>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
