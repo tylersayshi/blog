cda7f58040ccfd71d13c033ece8ed56b912b0ab0
Tyler <tyler@secondspectrum.com>
2025-02-25 13:45:36 -0800

    update analytics

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -3,14 +3,21 @@
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 import { useEffect } from "react";
+import { SuspendedPostHogPageView as PostHogPageView } from "./posthog-pageview";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init("phc_Freu7CLoutNnJBefDxniHNOm9lMChMpMU7Az17Kvfhe", {
       api_host: "https://us.i.posthog.com",
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      capture_pageleave: true,
     });
   }, []);
 
-  return <PHProvider client={posthog}>{children}</PHProvider>;
+  return (
+    <PHProvider client={posthog}>
+      <PostHogPageView />
+      {children}
+    </PHProvider>
+  );
 }
---
+++ src/components/posthog-pageview.tsx
@@ -0,0 +1,37 @@
+// app/PostHogPageView.tsx
+"use client";
+
+import { useRouter_UNSTABLE } from "waku";
+import { useEffect, Suspense } from "react";
+import { usePostHog } from "posthog-js/react";
+
+function PostHogPageView(): null {
+  const router = useRouter_UNSTABLE();
+  const posthog = usePostHog();
+
+  // Track pageviews
+  useEffect(() => {
+    const searchParams = router.query;
+    if (router.path && posthog) {
+      let url = window.origin + router.path;
+      if (searchParams) {
+        url = url + `?${searchParams}`;
+      }
+
+      posthog.capture("$pageview", { $current_url: url });
+    }
+  }, [router.path, router.query, posthog]);
+
+  return null;
+}
+
+// Wrap this in Suspense to avoid the `useSearchParams` usage above
+// from de-opting the whole app into client-side rendering
+// See: https://nextjs.org/docs/messages/deopted-into-client-rendering
+export function SuspendedPostHogPageView() {
+  return (
+    <Suspense fallback={null}>
+      <PostHogPageView />
+    </Suspense>
+  );
+}
