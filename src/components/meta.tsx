29d658706a5e741908efb27cd498d64d40d74f70
Tyler <26290074+thegitduck@users.noreply.github.com>
2025-02-24 20:35:26 -0800

    move og meta

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -12,19 +12,6 @@ export const Meta = ({ title, description }: MetaProps) => {
       <meta property="og:description" content={description} />
 
       <meta name="description" content="An internet website!" />
-
-      <meta property="og:url" content="https://tylur.blog" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="tyler's blog" />
-      <meta property="og:description" content="An internet website!" />
-      <meta property="og:image" content="/tylur.svg" />
-
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="tylur.blog" />
-      <meta property="twitter:url" content="https://tylur.blog" />
-      <meta name="twitter:title" content="tyler's blog" />
-      <meta name="twitter:description" content="An internet website!" />
-      <meta name="twitter:image" content="/tylur.svg" />
     </>
   );
 };
