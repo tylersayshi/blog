8b0067f8f1b7059e8bfae5e7a1fd62479210847a
Tyler <26290074+tylersayshi@users.noreply.github.com>
2025-03-30 11:46:01 -0700

    fix links

--- src/components/post-link.tsx
+++ src/components/post-link.tsx
@@ -1,21 +1,20 @@
 "use client";
 
-import { useRouter_UNSTABLE as useRouter } from "waku";
+import { Link, useRouter_UNSTABLE as useRouter } from "waku";
 import { PostFrontmatter } from "../types";
 
 export function PostLink({ post }: { post: PostFrontmatter }) {
-  const router = useRouter();
   return (
-    <a
-      href={`/${post.slug}`}
+    <Link
+      to={`/${post.slug}`}
       className="block p-4 w-full rounded-md border-2 border-black dark:border-white text-left hover:bg-slate-200 dark:hover:bg-gray-900"
-      onMouseEnter={() => router.prefetch(`/${post.slug}`)}
+      unstable_prefetchOnEnter
     >
       <div className="flex justify-between">
         <h2 className="text-xl font-bold">{post.title}</h2>
         <p className="text-sm ml-2 mt-1 whitespace-nowrap">{post.date}</p>
       </div>
       <p className="text-sm">{post.description}</p>
-    </a>
+    </Link>
   );
 }
